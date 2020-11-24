5cd7c8044ee6c95c89f5d96ddd1114f129a1b7d4
pc <bsse1129@iit.du.ac.bd>
2020-11-24 12:16:40 +0600

    #for finiding payment rate in Payment.py

---
+++ Introduction/Payment.py
@@ -0,0 +1,8 @@
+# This first line is provided for you
+#for finiding payment rate
+
+hrs = input("Enter Hours:")
+hrs = float(hrs)
+pay = input("Rate of Pay:")
+pay = float(pay)
+print('Pay:',pay*hrs)
