83da9f342214f1806c36155b44a016c351d3a591
pc <bsse1129@iit.du.ac.bd>
2020-11-24 10:26:37 +0600

    convertion between types in TypeConversion.py

---
+++ Introduction/TypeConversion.py
@@ -0,0 +1,13 @@
+
+aString = '1110'
+#convert to string
+aString = int(aString)
+aString=aString+19
+print(aString)
+
+aFloat = 99.455646
+bInt = int(aFloat)
+print(bInt)
+cFloat = bInt/aFloat
+print(cFloat)
+
