f3d0801677db46cabddf7f4035c8042449c29ea5
pc <bsse1129@iit.du.ac.bd>
2020-11-24 10:09:47 +0600

    features of variables and show  type name

---
+++ Introduction/Variables.py
@@ -0,0 +1,10 @@
+#features of variables
+
+eee='If you see the content : string'
+iii = ' to express some writings'
+
+rakibInt =1129
+print(eee+iii)
+print(type(eee))
+print(rakibInt)
+print(type(rakibInt))
