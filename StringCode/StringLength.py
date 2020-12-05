3d724da16f8ad2a0016bb049bb23027bfa545b1d
pc <bsse1129@iit.du.ac.bd>
2020-12-05 12:54:49 +0600

    create new StringLength.py to find length of string

---
+++ StringCode/StringLength.py
@@ -0,0 +1,8 @@
+flower = 'sunFlower'
+index = 0
+while index<len(flower):
+    letter = flower[index]
+    print(index,letter)
+    index = index+1
+
+print('String Length is :',index)
--- StringCode/StringPosition.py
+++ StringCode/StringPosition.py
@@ -5,3 +5,6 @@ print(cherecter)
 x=3
 dyname  = name[x]
 print(dyname)
+
+uniName = 'University of Dhaka'
+print("The lenth of my university name is", len(uniName))
