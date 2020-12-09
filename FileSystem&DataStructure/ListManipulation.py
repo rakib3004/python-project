fe83c595df09b652ae5be65cb67608f8b153c385
pc <bsse1129@iit.du.ac.bd>
2020-12-09 11:31:49 +0600

    create ListManipulation.py and manipulate ListManipulation.py

---
+++ FileSystem&DataStructure/ListManipulation.py
@@ -0,0 +1,19 @@
+from math import sqrt
+
+
+def findPrime(x):
+    aX=34
+    i=2
+    for i in range(sqrt(x)):
+        if(x%i == 0): return False
+
+    return True
+
+
+primeNumbers=[]
+iRange =2020
+for i in iRange :
+    if(findPrime(i)==True): primeNumbers.append(i)
+
+print(primeNumbers)
+
