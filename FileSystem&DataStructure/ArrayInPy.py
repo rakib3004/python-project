c2128a272e65f2a6f27c3fed81806185a0b6d35f
pc <bsse1129@iit.du.ac.bd>
2020-12-09 11:06:57 +0600

    create ArrayInPy.py and write code of different types of Array here.

---
+++ FileSystem&DataStructure/ArrayInPy.py
@@ -0,0 +1,8 @@
+friendsName = ['Shihab','Arnob','Momen']
+print(friendsName)
+friendsPoint = [54.6,87,60.3]
+print(friendsPoint)
+friendsData=['Shihab','Rangpur',454.4,600]
+print(friendsData)
+friendsArray=[[1,2,3],[10,20,30],[100,200,300]]
+print(friendsArray)
