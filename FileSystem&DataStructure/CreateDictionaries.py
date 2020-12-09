9642f52bd48d6d5f0e81f2f1fe97d284fb7ccac6
pc <bsse1129@iit.du.ac.bd>
2020-12-09 23:03:52 +0600

    create CreateDictionaries.py to add dictionaries elementary this just like add item and value in there

---
+++ FileSystem&DataStructure/CreateDictionaries.py
@@ -0,0 +1,8 @@
+tuitionMania = dict()
+tuitionMania['Rakib']=15000
+tuitionMania['Mohsin']=20000
+tuitionMania['Fahad']=23000
+tuitionMania['Musfiq']=14000
+print(tuitionMania)
+tuitionMania['Rakib'] = tuitionMania['Mohsin'] -5500
+print(tuitionMania)
