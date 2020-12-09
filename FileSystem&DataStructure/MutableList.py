595f0295f5cc82d991afe64f1f7fbfee5713f87f
pc <bsse1129@iit.du.ac.bd>
2020-12-09 11:15:36 +0600

    add MutableList.py and code of array and mutable size

---
+++ FileSystem&DataStructure/MutableList.py
@@ -0,0 +1,8 @@
+githubCommits = [1445,456,945,124,2375,1454]
+print('Rakib Previous Contribution/Commits:',githubCommits[4])
+githubCommits[4]=3745
+print('Rakib Present Contribution/Commits:',githubCommits[4])
+print("Array Size is :",len(githubCommits))
+print("They are : ")
+for iGithub in githubCommits:
+    print(iGithub)
