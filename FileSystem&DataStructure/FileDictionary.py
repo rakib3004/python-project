3ca350d37a67afdd438b43d9ba2f385f3f40dc97
pc <bsse1129@iit.du.ac.bd>
2020-12-09 23:58:38 +0600

    create FileDictionary.py to count how many each word on the text count the frequency and show them in a line from romeo-short.txt

---
+++ FileSystem&DataStructure/FileDictionary.py
@@ -0,0 +1,14 @@
+iFile = input('Enter the file name')
+file = open(iFile)
+
+iCountWords = dict()
+
+for iItem in file:
+    iText = iItem.split()
+    for jItem in iText:
+        iCountWords[jItem]=iCountWords.get(jItem,0)+1
+
+
+
+for iKey, iItem in iCountWords.items():
+    print(iKey,iItem)
