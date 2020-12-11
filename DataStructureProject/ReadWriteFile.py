2d4682b5d60cb9083dab9fab9377aab9111cc3b4
pc <bsse1129@iit.du.ac.bd>
2020-12-11 21:41:54 +0600

    create SplitDefinationWordByTuples.py and add function and other code

--- DataStructureProject/ReadWriteFile.py
+++ DataStructureProject/ReadWriteFile.py
@@ -1,3 +1,7 @@
+import random
+def get_word_definition(rawString):
+    tWord, tDefination = rawString.split(',',1)
+
 iFile = open("Vocabulary_list","r")
 
 iWordList = iFile.readlines()
@@ -7,3 +11,8 @@ iWordSet = set(iWordList)
 
 jFile = open("Vocabulary_set","w")
 jFile.writelines(iWordSet)
+
+iDictionaries = dict()
+for iString in iWordSet:
+    word,defination = get_word_definition(iWordSet)
+    
