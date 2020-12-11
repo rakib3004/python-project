6850f43ec172e0b93207caa21490672cc78c6883
pc <bsse1129@iit.du.ac.bd>
2020-12-11 21:45:36 +0600

    fixed code for ReadWriteFile.py and complete code in SplitDefinationWordByTuples.py to convert text on dictionary

--- DataStructureProject/ReadWriteFile.py
+++ DataStructureProject/ReadWriteFile.py
@@ -1,7 +1,3 @@
-import random
-def get_word_definition(rawString):
-    tWord, tDefination = rawString.split(',',1)
-
 iFile = open("Vocabulary_list","r")
 
 iWordList = iFile.readlines()
@@ -11,8 +7,3 @@ iWordSet = set(iWordList)
 
 jFile = open("Vocabulary_set","w")
 jFile.writelines(iWordSet)
-
-iDictionaries = dict()
-for iString in iWordSet:
-    word,defination = get_word_definition(iWordSet)
-    
--- DataStructureProject/SplitDefinationWordByTuples.py
+++ DataStructureProject/SplitDefinationWordByTuples.py
@@ -1,9 +1,8 @@
 import random
 
-
 def get_word_definition(rawString):
     tWord, tDefination = rawString.split(',', 1)
-
+    return  tWord,tDefination
 
 iFile = open("Vocabulary_list", "r")
 
@@ -17,4 +16,7 @@ jFile.writelines(iWordSet)
 
 iDictionaries = dict()
 for iString in iWordSet:
-    word, defination = get_word_definition(iWordSet)
+    word, defination = get_word_definition(iString)
+    iDictionaries[word]=defination
+
+print(iDictionaries)
