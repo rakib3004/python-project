35c0a0ac9876a6f52c81990ce84b5143c939a137
pc <bsse1129@iit.du.ac.bd>
2020-12-11 18:24:42 +0600

    complete CountHour.py to count number of specific hours by sorted

--- FileSystem&DataStructure/CountHour.py
+++ FileSystem&DataStructure/CountHour.py
@@ -1,26 +1,27 @@
 name = input("Enter file:")
-if len(name) < 1 : name = "mbox-short.txt"
-handle = open(name)
-
-iRead = handle.read()
-
+if len(name) < 1 : name = "mbox-short"
+iRead = open(name)
+iInt=1
+iThings = dict()
 for iLoop in iRead :
     if iLoop.startswith('From'):
-        if iLoop.find('@') and iLoop.find(':'):
-            iSentence = iLoop.strip()
-            iWords = iSentence.split()
-            iThings = iWords[4]
+        if iLoop.find('@') and iLoop.find('2008'):
+            if iInt == 1:
+                iWords = iLoop.split()
+                iInt = 2
+                iGot = iWords[5]
+                iGet = iGot[:2]
+                iThings[iGet] = iThings.get(iGet, 0)+1
+            else:
+                iInt = 1
+                continue
 
+aList = list()
+for a,b in iThings.items():
+    iTruples = (a, b)
+    aList.append(iTruples)
 
-iCollector = dict()
+aList = sorted(aList ,reverse=False)
 
-for iDir in iThings:
-    iCollector[iDir] = iCollector.get(iDir,0)+1
-
-
-iList = list()
-
-iList = iCollector.items()
-
-iList = sorted(iList,reverse=False)
-
+for c,f in aList:
+    print(c,f)
