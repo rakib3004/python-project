8c119538b4b1185939419639f0c64cea560fa1d9
pc <bsse1129@iit.du.ac.bd>
2020-12-17 02:24:06 +0600

    complete code in FindSumFromWeb.py

--- RegularExpression/FindSumFromWeb.py
+++ RegularExpression/FindSumFromWeb.py
@@ -1,14 +1,18 @@
 import urllib.request
-import  re
-url = urllib.request.urlopen('http://py4e-data.dr-chuck.net/regex_sum_42.txt')
+import re
+url = urllib.request.urlopen('http://py4e-data.dr-chuck.net/regex_sum_1081148.txt')
 sum=0
-for iterator in url:
+intList = list()
+for jterator in url:
+    iterator = str(jterator)
+    intList.append(iterator)
+    # iterator = iterator.decode('uft-8')
     intFile = re.findall('[0-9]+', iterator)
-    #for itFile in intFile:
-   # number = int(intFile)
-    print(intFile)
- #   print(number)
-#    sum = sum+number
+    for itFile in intFile:
+       number = int(itFile)
+       # print(number)
+       sum = sum+number
 
-#print('sum:', sum)
-
+print('sum:', sum)
+# for iLoop in intList:
+#     print('---->',iLoop)
