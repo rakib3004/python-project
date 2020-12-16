2a59cdab2e0582bb1d38b5c27c52c0fc9e542f5c
pc <bsse1129@iit.du.ac.bd>
2020-12-16 11:41:14 +0600

    changes in FindSumFromWeb.py

--- RegularExpression/FindSumFromWeb.py
+++ RegularExpression/FindSumFromWeb.py
@@ -4,9 +4,11 @@ url = urllib.request.urlopen('http://py4e-data.dr-chuck.net/regex_sum_42.txt')
 sum=0
 for iterator in url:
     intFile = re.findall('[0-9]+', iterator)
-    for itFile in intFile:
-         number = int(itFile)
-         sum = sum+number
+    #for itFile in intFile:
+   # number = int(intFile)
+    print(intFile)
+ #   print(number)
+#    sum = sum+number
 
-print('sum:', sum)
+#print('sum:', sum)
 
