5d3a6b1d642cbe302cde8017f75d57cde2a13a08
pc <bsse1129@iit.du.ac.bd>
2020-12-17 23:15:53 +0600

    GET http response from server : code are written in Socket.py

--- Networking/Socket.py
+++ Networking/Socket.py
@@ -1,3 +1,13 @@
 import socket
-mySocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-mySocket.connect('data.pr4e.org', 80)
+mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mySocket.connect(('data.pr4e.org', 80 ))
+commandLine ='GET http://data.pr4e.org/romeo.txt HTTP1.0\n\n'.encode()
+mySocket.send(commandLine)
+
+while True :
+    data = mySocket.recv(512)
+    if(len(data)<1):
+        break
+    else:
+        print(data.decode())
+mySocket.close()
