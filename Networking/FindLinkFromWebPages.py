c0053ec2767b8f6d58323c3f1cb788ffeb781f53
pc <bsse1129@iit.du.ac.bd>
2020-12-19 03:29:07 +0600

    add new content to FindLinkFromWebPages.py and replace file like test_builder_registry.py, test_docs.py etc.

--- Networking/FindLinkFromWebPages.py
+++ Networking/FindLinkFromWebPages.py
@@ -1,1 +1,22 @@
-import urllib.request, urllib.parse, urllib.error
+
+from  urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+#ignore SSl certificate errors
+createDefaultContext = ssl.create_default_context()
+createDefaultContext.check_hostname = False
+createDefaultContext.verify_mode =ssl.CERT_NONE
+
+inputURL = input('Enter URL:')
+html = urlopen(inputURL,context=createDefaultContext).read()
+beautifulSoup = BeautifulSoup(html, "html.parser")
+
+#Retrive all of the anchor tags
+
+iTags = beautifulSoup('a')
+for jTags in iTags:
+    print('TAG', jTags)
+    print('URL :',jTags.get('href',None))
+    print('Contents:',jTags.contents[0])
+    print('Attrs:', jTags.attrs)
