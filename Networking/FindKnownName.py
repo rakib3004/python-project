8f6f8762c20361ff47e52be4f00b50260473fddd
pc <bsse1129@iit.du.ac.bd>
2020-12-19 23:01:51 +0600

    create FindKnownName.py and add code to parse Web Link

--- Networking/FindKnownName.py
+++ Networking/FindKnownName.py
@@ -3,6 +3,8 @@ from  urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
 
+
+
 #ignore SSl certificate errors
 createDefaultContext = ssl.create_default_context()
 createDefaultContext.check_hostname = False
@@ -11,14 +13,20 @@ createDefaultContext.verify_mode =ssl.CERT_NONE
 inputURL = input('Enter URL:')
 html = urlopen(inputURL,context=createDefaultContext).read()
 beautifulSoup = BeautifulSoup(html, "html.parser")
-
+# intCount = int(input('Enter Count:'))
+# intPosition = int(input('Enter Position:'))
 #Retrive all of the anchor tags
-sum=0
-iTags = beautifulSoup('span')
+sum=1
+pos=1
+count=1
+selectPosition=18
+iTags = beautifulSoup('a')
 for jTags in iTags:
     # print('TAG', jTags)
-    # print('URL :',jTags.get('href',None))
+    if sum==selectPosition:
+       print('URL :',sum,jTags.get('href',None))
+       sum =sum+1
     # print('Contents:',jTags.contents[0])
-    sum =sum+int(jTags.contents[0])
-
-print(sum)
+#     sum =sum+int(jTags.contents[0])
+#
+# print(sum)
