cc9e53ad88b4899e8c37f31023746008b84bfeef
pc <bsse1129@iit.du.ac.bd>
2020-12-22 23:31:48 +0600

    add recursion methods and listing methods in FindKnownName.py

--- Networking/FindKnownName.py
+++ Networking/FindKnownName.py
@@ -3,21 +3,22 @@ from  urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
 
-pos=1
+
 iO='https://rakib3004.github.io'
 
-def parsename(inputURL):
+def parsename(inputList):
+    pos = int(inputList[1])
     if pos ==7:
-        return iO
+        return inputList
     else :
         pos=pos+1
         createDefaultContext = ssl.create_default_context()
         createDefaultContext.check_hostname = False
         createDefaultContext.verify_mode = ssl.CERT_NONE
 
-        html = urlopen(inputURL, context=createDefaultContext).read()
+        html = urlopen(inputList[0], context=createDefaultContext).read()
         beautifulSoup = BeautifulSoup(html, "html.parser")
-
+        inputList.clear()
         sum = 1
         selectPosition = 18
         iTags = beautifulSoup('a')
@@ -25,12 +26,14 @@ def parsename(inputURL):
             # print('TAG', jTags)
             if sum == selectPosition:
                 iLink = str(jTags.get('href', None))
-                getIO = parsename(iLink)
+                inputList.append(inputList)
+                inputList.append(pos)
+                getIO = parsename(inputList)
                 break
             else:
                 a = 0  # print('URL  :', sum, jTags.get('href', None))
             sum = sum + 1
-    return getIO
+    return inputList
 
 #ignore SSl certificate errors
 pos=1
@@ -44,6 +47,7 @@ beautifulSoup = BeautifulSoup(html, "html.parser")
 # intCount = int(input('Enter Count:'))
 # intPosition = int(input('Enter Position:'))
 #Retrive all of the anchor tags
+inputList = list()
 sum=1
 selectPosition=18
 iTags = beautifulSoup('a')
@@ -51,10 +55,12 @@ for jTags in iTags:
     # print('TAG', jTags)
     if sum==selectPosition:
         iLink = str(jTags.get('href',None))
-        getIO = parsename(iLink)
+        inputList.append(inputList)
+        inputList.append(1)
+        inputList = parsename(inputList)
         break
     else:
        a=0 #print('URL  :', sum, jTags.get('href', None))
     sum =sum+1
 
-print(getIO)
+print(inputList)
