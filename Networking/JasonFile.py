38c1e5560ffabb1c9a626a47c24b8b9529011e3e
pc <bsse1129@iit.du.ac.bd>
2020-12-26 15:21:09 +0600

    create JasonFile.py and convert json data to readable python and text data

---
+++ Networking/JasonFile.py
@@ -0,0 +1,17 @@
+import json
+
+data = '''{
+"name" : "Rakib",
+"phone " : {
+"type" :  "intl",
+"number " : "11803004"
+},
+"email" : {
+"hide" : "yes"
+}
+}'''
+
+info = json.loads(data)
+
+print('Name :',info["name"])
+print('Hide :', info["email"]["hide"])
