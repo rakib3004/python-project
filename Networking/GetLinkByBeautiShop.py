adc19ff0695dba59473426ef6166af2dd2c7a520
pc <bsse1129@iit.du.ac.bd>
2021-02-06 10:44:24 +0600

    create GetLinkByBeautiShop.py and add code

---
+++ Networking/GetLinkByBeautiShop.py
@@ -0,0 +1,25 @@
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+iter=1
+for tag in tags:
+    print('position',iter,':',tag.get('href', None))
+    iter=iter+1
+    if(iter==20) :
+        break
