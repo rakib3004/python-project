141e95ad5d52d4330d80e4c7aa9a14115c56b93f
pc <bsse1129@iit.du.ac.bd>
2021-02-06 11:04:05 +0600

    create simple CompareNumber.py code in Basic of python

---
+++ Basic/CompareNumber.py
@@ -0,0 +1,29 @@
+print('Decisions, decisions')
+first = input('Please enter the first number:')
+second = input('Please enter the second number:')
+
+if(second==first):
+    print(second, 'is equal to', first)
+else:
+    print(second, 'is not equal to', first)
+
+if(second>first):
+    print(second, 'is greater than', first)
+else:
+    print(second, 'is not greater than', first)
+
+if(second<first):
+    print(second, 'is less than', first)
+else:
+    print(second, 'is not less than', first)
+
+if(second>=first):
+    print(second, 'is greater than or equal to', first)
+else:
+    print(second, 'is greater than or not equal to', first)
+
+if(second<=first):
+    print(second, 'is less than or equal to', first)
+else:
+    print(second, 'is less than or not equal to', first)
+
