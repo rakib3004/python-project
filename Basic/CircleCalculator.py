f53d5c20174e17455eb3ce3a36f214f50da8fda0
pc <bsse1129@iit.du.ac.bd>
2021-02-06 11:57:56 +0600

    write code in CircleCalculator.py to find diameter, circumference and area of a circle

---
+++ Basic/CircleCalculator.py
@@ -0,0 +1,8 @@
+print('Circle Calculator')
+print('*****************')
+pi = 3.1415926535897932384626
+radius = float(input('Enter the radius:'))
+
+print('Diameter is :',2*radius)
+print('Circumference is :',2*pi*radius)
+print('Area is :',pi*radius*radius)
