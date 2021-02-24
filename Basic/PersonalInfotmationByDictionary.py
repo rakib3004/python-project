f4182b9feb715017cf63fec9b6e577e81e2df8f4
rakib3004 <bsse1129@iit.du.ac.bd>
2021-02-24 22:13:38 +0600

    write code in PersonalInfotmationByDictionary.py

---
+++ Basic/PersonalInfotmationByDictionary.py
@@ -0,0 +1,13 @@
+myProfile = dict()
+
+myProfile['Name']='Josh'
+myProfile['Height']='1.6'
+myProfile['Eye Colour']='Blue'
+myProfile['Hair Colour']='Brown'
+myProfile['Birth Place']='Manchester'
+
+for attr,val in myProfile.items():
+    print(attr, 'is', val)
+
+
+
