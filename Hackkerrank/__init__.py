68bcafe753ddf13af71ba2820266cd7cda1055d9
rakib3004 <bsse1129@iit.du.ac.bd>
2021-03-06 11:21:01 +0600

    change code in __init__.py

--- Hackkerrank/__init__.py
+++ Hackkerrank/__init__.py
@@ -7,7 +7,13 @@ import sys
 
 if __name__ == '__main__':
     n = int(input().strip())
-    if (n%2==1|(n>=6&n<=20)):
+    if(n%2==0):
+        if(n<=5&n>=2):
+            print('Not Weird')
+        elif(n<=20&n>=6):
+            print('Weird')
+        else:
+            print('Not Weird')
+    elif(n%2==1):
         print('Weird')
-    else:
-        print('Not Weird')
+
