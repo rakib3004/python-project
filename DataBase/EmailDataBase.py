08011db76c8288f7565c0ec4cc605644e0a0ec8f
rakib3004 <bsse1129@iit.du.ac.bd>
2021-03-10 17:30:45 +0600

    add new file and modify sql commands

--- DataBase/EmailDataBase.py
+++ DataBase/EmailDataBase.py
@@ -3,29 +3,31 @@ import sqlite3
 conn = sqlite3.connect('emaildb.sqlite')
 cur = conn.cursor()
 
+iEmail = 'stephen.marquard@uct.ac.za'
+
 cur.execute('DROP TABLE IF EXISTS Counts')
 
-#cur.execute("CREATE TABLE Counts (email TEXT, count INTEGER)")
+cur.execute("CREATE TABLE Counts (email TEXT, count INTEGER)")
 
 fname = input('Enter file name: ')
-if (len(fname) < 1): fname = 'mbox-short'
+if (len(fname) < 1): fname = 'data'
 fh = open(fname)
 for line in fh:
     if not line.startswith('From: '): continue
     pieces = line.split()
     email = pieces[1]
-    cur.execute('SELECT count FROM Counts WHERE email = ? ', (email,))
+    cur.execute('''SELECT count FROM Counts WHERE email = 'stephen.marquard@uct.ac.za' ''', (email,))
     row = cur.fetchone()
     if row is None:
         cur.execute('''INSERT INTO Counts (email, count)
-                VALUES (?, 1)''', (email,))
+                VALUES ('stephen.marquard@uct.ac.za', 1)''', (email,))
     else:
-        cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?',
+        cur.execute('''UPDATE Counts SET count = count + 1 WHERE email = 'stephen.marquard@uct.ac.za';''',
                     (email,))
     conn.commit()
 
 # https://www.sqlite.org/lang_select.html
-sqlstr = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
+sqlstr = '''SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'''
 
 for row in cur.execute(sqlstr):
     print(str(row[0]), row[1])
