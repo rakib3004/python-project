1e5a23b745e3ed47f04b7ccaee31ebdf37bbdc5e
rakib3004 <bsse1129@iit.du.ac.bd>
2021-03-10 14:31:49 +0600

    add mbox-short and emaildb.sqlite

--- DataBase/EmailDataBase.py
+++ DataBase/EmailDataBase.py
@@ -1,11 +1,33 @@
 import sqlite3
-connection = sqlite3.connect('email.sqlite')
-current = connection.cursor()
 
-current.execute('''
-DROP TABLE IF EXISTS Department''')
+conn = sqlite3.connect('emaildb.sqlite')
+cur = conn.cursor()
 
-current.execute('''
-CREATE TABLE Department(DeptName Text, StuNo INTEGER )''')
+cur.execute('DROP TABLE IF EXISTS Counts')
 
-fileName = input('Enter File Name')
+#cur.execute("CREATE TABLE Counts (email TEXT, count INTEGER)")
+
+fname = input('Enter file name: ')
+if (len(fname) < 1): fname = 'mbox-short'
+fh = open(fname)
+for line in fh:
+    if not line.startswith('From: '): continue
+    pieces = line.split()
+    email = pieces[1]
+    cur.execute('SELECT count FROM Counts WHERE email = ? ', (email,))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (email, count)
+                VALUES (?, 1)''', (email,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?',
+                    (email,))
+    conn.commit()
+
+# https://www.sqlite.org/lang_select.html
+sqlstr = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
