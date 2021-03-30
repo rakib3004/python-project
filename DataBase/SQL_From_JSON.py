e188eee48842ca6571bb57cb9a82bc7fd23fd176
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-03-30 18:06:27 +0600

    add new json file

--- DataBase/SQL_From_JSON.py
+++ DataBase/SQL_From_JSON.py
@@ -9,17 +9,14 @@ cur.executescript('''
 DROP TABLE IF EXISTS User;
 DROP TABLE IF EXISTS Member;
 DROP TABLE IF EXISTS Course;
-
 CREATE TABLE User (
     id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     name   TEXT UNIQUE
 );
-
 CREATE TABLE Course (
     id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     title  TEXT UNIQUE
 );
-
 CREATE TABLE Member (
     user_id     INTEGER,
     course_id   INTEGER,
@@ -30,7 +27,7 @@ CREATE TABLE Member (
 
 fname = input('Enter file name: ')
 if len(fname) < 1:
-    fname = 'roster_data_sample.json'
+    fname = 'roster_data.json'
 
 # [
 #   [ "Charley", "si110", 1 ],
@@ -43,21 +40,22 @@ for entry in json_data:
 
     name = entry[0]
     title = entry[1]
+    role = entry[2]
 
-    print((name, title))
+    print(name, title)
 
     cur.execute('''INSERT OR IGNORE INTO User (name)
-        VALUES ( ? )''', ( name, ) )
+        VALUES ( ? )''', (name, ))
     cur.execute('SELECT id FROM User WHERE name = ? ', (name, ))
     user_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR IGNORE INTO Course (title)
-        VALUES ( ? )''', ( title, ) )
+        VALUES ( ? )''', (title, ))
     cur.execute('SELECT id FROM Course WHERE title = ? ', (title, ))
     course_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR REPLACE INTO Member
-        (user_id, course_id) VALUES ( ?, ? )''',
-        ( user_id, course_id ) )
+        (user_id, course_id, role) VALUES ( ?, ?, ? )''',
+        (user_id, course_id, role))
 
     conn.commit()
