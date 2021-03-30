a480dbd37151f764a13d10cf5af60a0d582dfe53
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-03-31 01:05:12 +0600

    change variable name from email to org

--- DataBase/EmailDataBase.py
+++ DataBase/EmailDataBase.py
@@ -1,33 +1,34 @@
 import sqlite3
 
-conn = sqlite3.connect('emaildb.sqlite')
+conn = sqlite3.connect('orgdb.sqlite')
 cur = conn.cursor()
-
-iEmail = 'stephen.marquard@uct.ac.za'
 
 cur.execute('DROP TABLE IF EXISTS Counts')
 
-cur.execute("CREATE TABLE Counts (email TEXT, count INTEGER)")
+cur.execute('''
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
 
-fname = input('Enter file name: ')
-if (len(fname) < 1): fname = 'data'
+fname = 'mbox.txt'
+if (len(fname) < 1): fname = 'mbox.txt'
 fh = open(fname)
 for line in fh:
     if not line.startswith('From: '): continue
     pieces = line.split()
-    email = pieces[1]
-    cur.execute('''SELECT count FROM Counts WHERE email = 'stephen.marquard@uct.ac.za' ''', (email,))
+    org = pieces[1]
+    parts = org.split('@')
+    org=[1]
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
     row = cur.fetchone()
     if row is None:
-        cur.execute('''INSERT INTO Counts (email, count)
-                VALUES ('stephen.marquard@uct.ac.za', 1)''', (email,))
+        cur.execute('''INSERT INTO Counts (org, count)
+                VALUES (?, 1)''', (org,))
     else:
-        cur.execute('''UPDATE Counts SET count = count + 1 WHERE email = 'stephen.marquard@uct.ac.za';''',
-                    (email,))
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                    (org,))
     conn.commit()
 
 # https://www.sqlite.org/lang_select.html
-sqlstr = '''SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'''
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
 
 for row in cur.execute(sqlstr):
     print(str(row[0]), row[1])
