04d4a7c3b40001147422c853c8474139d1138177
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-04-09 15:54:44 +0600

    change and add in RetrivingGeoData.jpg, TextualGeoData.jpg and 7 others

--- MusicalTraceDatabase/tracks.py
+++ MusicalTraceDatabase/tracks.py
@@ -7,36 +7,34 @@ cur = conn.cursor()
 # Make some fresh tables using executescript()
 cur.executescript('''
 DROP TABLE IF EXISTS Artist;
+DROP TABLE IF EXISTS Genre; 
 DROP TABLE IF EXISTS Album;
 DROP TABLE IF EXISTS Track;
-
 CREATE TABLE Artist (
     id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     name    TEXT UNIQUE
 );
-
 CREATE TABLE Genre (
     id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     name    TEXT UNIQUE
 );
-
 CREATE TABLE Album (
     id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     artist_id  INTEGER,
     title   TEXT UNIQUE
 );
-
 CREATE TABLE Track (
     id  INTEGER NOT NULL PRIMARY KEY 
         AUTOINCREMENT UNIQUE,
     title TEXT  UNIQUE,
     album_id  INTEGER,
+    genre_id  INTEGER,
     len INTEGER, rating INTEGER, count INTEGER
 );
 ''')
 
 
-fname ='Library.xml'
+fname = 'Library.xml'
 if ( len(fname) < 1 ) : fname = 'Library.xml'
 
 # <key>Track ID</key><integer>369</integer>
@@ -59,31 +57,30 @@ for entry in all:
     name = lookup(entry, 'Name')
     artist = lookup(entry, 'Artist')
     album = lookup(entry, 'Album')
-    genre = lookup(entry, 'Genre')
+    #added this
+    genre = lookup(entry,'Genre')
     count = lookup(entry, 'Play Count')
     rating = lookup(entry, 'Rating')
     length = lookup(entry, 'Total Time')
 
-    if name is None or artist is None or album is None or genre is None :
+    if name is None or artist is None or album is None or genre is None:
         continue
 
-    print(name, artist, album,genre,count, rating, length)
+    print (name, artist, album, genre, count, rating, length)
 
     cur.execute('''INSERT OR IGNORE INTO Artist (name) 
         VALUES ( ? )''', ( artist, ) )
-    cur.execute('''INSERT OR IGNORE INTO Genre (name) 
-        VALUES ( ? )''', ( genre, ) )
-
     cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
     artist_id = cur.fetchone()[0]
-
+    #Added this
+    cur.execute('''INSERT OR IGNORE INTO Genre (name) 
+        VALUES ( ? )''', ( genre, ) )
     cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
     genre_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
         VALUES ( ?, ? )''', ( album, artist_id ) )
     cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
-
     album_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR REPLACE INTO Track
@@ -91,11 +88,12 @@ for entry in all:
         VALUES ( ?, ?, ?, ?, ?, ? )''',
         ( name, album_id, genre_id, length, rating, count ) )
 
-    cur.execute('''SELECT Track.title, Artist.name, Album.title, Genre.name 
-       FROM Track JOIN Genre JOIN Album JOIN Artist 
-       ON Track.genre_id = Genre.ID and Track.album_id = Album.id 
-           AND Album.artist_id = Artist.id
-       ORDER BY Artist.name LIMIT 3''')
-
     conn.commit()
-sqlstr = 'SELECT Track.title, Artist.name, Album.title, Genre.name FROM Track JOIN Genre JOIN  Album JOIN  Artist'
+#Added this
+sqlstr = 'SELECT Track.title, Artist.name, Album.title, Genre.name FROM Track JOIN Genre JOIN Album JOIN Artist ON Track.genre_id = Genre.ID and Track.album_id = Album.id AND Album.artist_id = Artist.id ORDER BY Artist.name LIMIT 3'
+
+print
+for row in cur.execute(sqlstr) :
+    print (str(row[0]),str(row[1]),str(row[2]),str(row[3]))
+
+cur.close()
--- geodata/geodump.py
+++ geodata/geodump.py
@@ -6,11 +6,11 @@ conn = sqlite3.connect('geodata.sqlite')
 cur = conn.cursor()
 
 cur.execute('SELECT * FROM Locations')
-fhand = codecs.open('where.js','w', "utf-8")
+fhand = codecs.open('where.js', 'w', "utf-8")
 fhand.write("myData = [\n")
 count = 0
 for row in cur :
-    data = str(row[1])
+    data = str(row[1].decode())
     try: js = json.loads(str(data))
     except: continue
 
@@ -20,9 +20,9 @@ for row in cur :
     lng = js["results"][0]["geometry"]["location"]["lng"]
     if lat == 0 or lng == 0 : continue
     where = js['results'][0]['formatted_address']
-    where = where.replace("'","")
+    where = where.replace("'", "")
     try :
-        print where, lat, lng
+        print(where, lat, lng)
 
         count = count + 1
         if count > 1 : fhand.write(",\n")
@@ -34,6 +34,6 @@ for row in cur :
 fhand.write("\n];\n")
 cur.close()
 fhand.close()
-print count, "records written to where.js"
-print "Open where.html to view the data in a browser"
+print(count, "records written to where.js")
+print("Open where.html to view the data in a browser")
 
