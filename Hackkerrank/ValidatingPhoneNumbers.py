ae8880c21cd0b860033c55d16a41734c951e7eb6
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-04-19 19:48:06 +0600

    find out valid number which contain 10 digits and start with 7|8|9

--- Hackkerrank/ValidatingPhoneNumbers.py
+++ Hackkerrank/ValidatingPhoneNumbers.py
@@ -5,13 +5,15 @@ i =0
 for i in range(n):
     iNumber = str(input())
     i10 = len(iNumber)
-    iF = re.findall('^(7|8|9)', iNumber)
+    iF = re.findall('^(7|8|9)[0-9]{9}', iNumber)
     #print(iF)
     if(iF==[]):
         print('NO')
     else:
         if(i10==10):
             print('YES')
+        else:
+            print('NO')
 
 
 
