35e5003555d69e8fca2c6bf76e15e518ab2d63f5
rakib3004 <bsse1129@iit.du.ac.bd>
2021-06-26 16:57:40 +0600

    change code in Capitalize.py

--- Hackkerrank/Capitalize.py
+++ Hackkerrank/Capitalize.py
@@ -9,22 +9,45 @@ import sys
 
 # Complete the solve function below.
 def solve(s):
+
+
     names = list()
     names = s.split()
-    firstName = str(names[0])
-    lastName = str(names[1])
-    firstName=firstName.capitalize()
-    lastName=lastName.capitalize()
+    n=len(names)
+    fullName=''
 
-    return  firstName+" "+lastName
+    for name in names:
+        #print(name)
+        firstName = str(name)
+        first=firstName.capitalize()
+        #print(first)
+        fullName=fullName+first+'.'
+
+    return fullName
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     s = input()
 
+
+    n=0
     result = solve(s)
 
-    fptr.write(result + '\n')
+    for r in result:
+        if(r!='.'):
+            print(r,end='')
+        else:
+            for k in range(n,len(s)):
+                if (s[k] == ' '):
+                    print(s[k])
+                else:
+                    n=k+1
+                break
 
-    fptr.close()
+
+
+    print(result)
+    #fptr.write(result + '\n')
+
+    #fptr.close()
