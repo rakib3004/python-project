54a9b37f0981126cc7966a74c20392a6492c6b6c
rakib3004 <bsse1129@iit.du.ac.bd>
2021-08-27 23:49:00 +0600

    add new file BanglaRemoval.py and GoogleCache.txt

---
+++ LibraryAssistantTool/BanglaRemoval.py
@@ -0,0 +1,12 @@
+iFile = open("GoogleCache.txt", "r")
+
+jafarIqbalList = iFile.read()
+print(jafarIqbalList)
+# print(iWordList)
+
+#for c in jafarIqbalList:
+#    if (ord(c)>65 and ord(c)<122):
+#        f = open("JafarIqbal.txt", "w")
+#        f.write(c)
+#        f.close()
+
