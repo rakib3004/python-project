4b62816383661070e7612a1c94cc417a888fe0e0
rakib3004 <bsse1129@iit.du.ac.bd>
2021-11-07 22:12:44 +0600

    add new client handle

--- SocketProgramming/server.py
+++ SocketProgramming/server.py
@@ -3,4 +3,24 @@ import  threading
 
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
-print(socket.gethostname())
+
+ADDR = (SERVER,PORT)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print()
+
+def start():
+    server.listen()
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn,addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount()-1}")
+
+print("[STARTING] server is starting...")
+start()
+
+
