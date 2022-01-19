393c5ffe13f16ca64c74a6ab8b105cdd9c919965
rakib3004 <bsse1129@iit.du.ac.bd>
2022-01-19 17:37:04 +0600

    add middle man

--- DataMining/ImageReader.py
+++ DataMining/ImageReader.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
-im = Image.open('0016.jpg') # Can be many different formats.
-maskIm  = Image.open('0016.bmp') # Can be many different formats.
+im = Image.open('0000.jpg') # Can be many different formats.
+maskIm  = Image.open('0000.bmp') # Can be many different formats.
 
 imageLoader = im.load()
 maskImageLoader = maskIm.load()
@@ -12,6 +12,8 @@ width = maskIm.size[1]  # Get the width and hight of the image for iterating ove
 
 skin=[[[0]*256]*256]*256
 nonSkin=[[[0]*256]*256]*256
+trainedData=[[[0]*256]*256]*256
+
 skinCount=0
 nonSkinCount=0
 
@@ -20,7 +22,7 @@ for i in range(height):
     for j in range(width): 
         print(maskImageLoader[i,j],end="compare to ")
         print(maskImageLoader[i,j])
-        if(maskImageLoader[i,j][0]<220&maskImageLoader[i,j][1]<220&maskImageLoader[i,j][2]<220):
+        if(imageLoader[i,j][0]<255&imageLoader[i,j][1]<255&imageLoader[i,j][2]<255):
             print(imageLoader[i,j])
             skin[imageLoader[i,j][0]][imageLoader[i,j][1]][imageLoader[i,j][2]]+=1
             skinCount+=1
@@ -31,4 +33,22 @@ for i in range(height):
 
         print("Skin Count: ",skinCount, "Non Skin Count: ", nonSkinCount)     
 
-            
+
+for i in range(256):
+    for j in range(256):
+        for k in range (256):
+            skin[i][j][k]/=skinCount
+            nonSkin[i][j][k]/=nonSkinCount
+            if(skin[i][j][k]==0.0 and nonSkin[i][j][k]==0.0):
+                trainedData[i][j][k]=0
+            elif(nonSkin[i][j][k]==0.0):
+                trainedData[i][j][k]=100
+            else:
+                trainedData[i][j][k]= nonSkin[i][j][k] and skin[i][j][k]/nonSkin[i][j][k]
+
+for i in range(256):
+    for j in range(256):
+        for k in range (256):
+            print((i,j,k),float(trainedData[i][j][k]))
+
+
