2fcdfd83ed3be331410e627903ddd8b1c7859097
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-26 01:23:02 +0600

    add new student

---
+++ DataWareHouse/__init__.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self, firstName, lastName, roll, department, email, mobile, gender, dictrict):
+        self.firstName=firstName
+        self.lastName=lastName
+        self.roll = roll
+        self.department = department
+        self.email = email
+        self.mobile = mobile
+        self.gender = gender
+        self.dictrict = dictrict
