38d72d8a716a851f1f9ea0264341c386fdff7546
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-26 16:03:29 +0600

    complete halloween

--- Halloween/HalloweenGame.py
+++ Halloween/HalloweenGame.py
@@ -1,6 +1,20 @@
-
 import pygame
+from time import  sleep
 
-print()
+pygame.init()
+
+window = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+pygame.mixer.init()
+pygame.mixer.music.load('ratsasan.mp3')
+pygame.mixer.music.play()
+sleep(8)
+pygame.mixer.music.load('scary.mp3')
+pygame.mixer.music.play()
+sleep(1)
+image = pygame.image.load('halloween.png')
+window.blit(image, (0,0))
+pygame.display.update()
+sleep(3)
+
 
 
