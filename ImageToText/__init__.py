0ab91e73e94b99ebb7712c94a19cf0fbd299ea6a
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-02-21 17:53:42 +0600

    unsucessful code to bangla ocr

--- ImageToText/__init__.py
+++ ImageToText/__init__.py
@@ -1,4 +1,14 @@
-from PIL import Image
-import pytesseract as tess
+import pytesseract
+from PIL import Image, ImageEnhance, ImageFilter
 
-print(tess.image_to_string(Image.open('Scrum4.png'), lang='ben'))
+im = Image.open("Scrum4.png") # the second one
+im = im.filter(ImageFilter.MedianFilter())
+enhancer = ImageEnhance.Contrast(im)
+im = enhancer.enhance(2)
+im = im.convert('1')
+im.save('temp2.png')
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
+print('ok')
+text = pytesseract.image_to_string(Image.open('Scrum4.png'), lang="ben")
+print('ok')
+print(text)
