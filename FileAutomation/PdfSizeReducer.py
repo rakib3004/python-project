5b57bca3f9291ef87a7c9b5e7b7d649b0446c29d
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-08-13 00:19:44 +0600

    add new PDF reducer code

--- FileAutomation/PdfSizeReducer.py
+++ FileAutomation/PdfSizeReducer.py
@@ -1,45 +1,47 @@
 import os
-import fitz  # PyMuPDF
+from PyPDF2 import PdfReader, PdfWriter
+from io import BytesIO
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
 
-
-def reduce_pdf_size(input_path, output_path, quality=50):
+def reduce_pdf_size(input_path, output_path, compression=0):
     try:
-        # Open the input PDF file
-        pdf_document = fitz.open(input_path)
+        # Read the input PDF
+        reader = PdfReader(input_path)
 
-        # Initialize the output PDF document
-        pdf_document_resized = fitz.open()
+        # Create an output PDF writer
+        pdf_writer = PdfWriter()
 
-        # Iterate through all pages and copy them with reduced quality to the new document
-        for page_num in range(pdf_document.page_count):
-            page = pdf_document.load_page(page_num)
-            new_page = pdf_document_resized.new_page(width=page.width, height=page.height)
-            new_page.show_pdf_page(page)
+        # Iterate through each page and add it to the output PDF
+        for page_num in range(len(reader.pages)):
+            output_page = pdf_writer.add_page(reader.pages[page_num])
+            output_page.compress_content(compression)
 
-        # Save the new PDF with reduced size
-        pdf_document_resized.save(output_path, deflate=True, quality=quality)
+        # Save the output PDF to a BytesIO buffer
+        output_buffer = BytesIO()
+        pdf_writer.write(output_buffer)
+        output_buffer.seek(0)
 
-        # Close the documents
-        pdf_document.close()
-        pdf_document_resized.close()
+        # Create a canvas for the output PDF
+        output_canvas = canvas.Canvas(output_path, pagesize=letter)
+        output_canvas.drawString(100, 100, "Reduced PDF using ReportLab")
+        output_canvas.save()
+
+        # Merge the compressed content and the canvas
+        with open(output_path, 'wb') as output_file:
+            output_file.write(output_buffer.read())
 
         print(f"Reduced file: {input_path} -> {output_path}")
     except Exception as e:
         print(f"Error: {e}")
 
-
 def process_pdf_folder(folder_path):
     for filename in os.listdir(folder_path):
         if filename.endswith(".pdf"):
             input_path = os.path.join(folder_path, filename)
-            output_path = os.path.join(folder_path, "resized_" + filename)
+            output_path = os.path.join(folder_path, "reduced_" + filename)
             reduce_pdf_size(input_path, output_path)
 
-            # Replace the original file with the resized one
-            os.remove(input_path)
-            os.rename(output_path, input_path)
-
-
 if __name__ == "__main__":
-    target_folder = r"C:\path\to\your\folder"  # Replace with your folder path
+    target_folder = r"D:\ObjectOrientedConcepts2Slides"  # Replace with your folder path
     process_pdf_folder(target_folder)
